334248e6e6b5ad286bbff850dcff928d3bb33811
RahulKumar8967 <sainirahul8967@gmail.com>
2023-11-25 10:55:14 +0530

    add all files

---
+++ DoubleClick/DoubleClick.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("dblclick", function (){
+alert("you have click me Double Time 👍");
+
+});
---
+++ MouseOver/Rahul.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("mouseover", function() {
+alert("you are entered in Restricted Area  🙅 (mouse over event is working properly)"
+);
+
+});
